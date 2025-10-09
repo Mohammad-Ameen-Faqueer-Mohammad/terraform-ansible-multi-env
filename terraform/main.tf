1d5dbee2956e170af454ee69dcf2f0107cd21112
Mohammad Ameen <117063224+Mohammad-Ameen-Faqueer-Mohammad@users.noreply.github.com>
2025-10-09 15:42:49 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,29 +1,29 @@
 # dev infra  -- 2 EC2 - 1 S3 1-dynamoDB
 module "dev-infra" {
-    source = "./My_infra"
+    source = "./infra"
     env= "dev"
-    instance_count=2
+    instance_count= 1
     instance_type= "t3.micro"
     ami="ami-02d26659fd82cf299"
     volume_size=10
 }
 #stag infra  -- 2 EC2 - 1 S3 1-dynamoDB
 module "staging-infra" {
-    source = ./My_infra"
+    source = "./infra"
     env= "staging"
-    instance_count=2
-    instance_type="t3.micro"
-    ami="ami-02d26659fd82cf299"
+    instance_count= 1
+    instance_type= "t3.micro"
+    ami= "ami-02d26659fd82cf299"
     volume_size= 8
 }
 
 #prod infra  -- 3 EC2 - 1 S3 1-dynamoDB
 module "Prod-infra" {
-    source = ./My_infra"
-    env= "Prod"
-    instance_count=3
-    instance_type="t3.micro"
-    ami="ami-02d26659fd82cf299"
+    source = "./infra"
+    env= "prod"
+    instance_count= 1
+    instance_type= "t3.micro"
+    ami= "ami-02d26659fd82cf299"
     volume_size= 8
 }
 
@@ -34,4 +34,4 @@ output "dev_infra_ec2_instance_public_ips" {
 output "staging_infra_ec2_instance_public_ips" {
     value = module.staging_infra.ec2_public_ip
 }
-
+~                     
