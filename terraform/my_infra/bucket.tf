40654813d2f3ab99fc3859f05312353bf7a728fe
Mohammad Ameen <117063224+Mohammad-Ameen-Faqueer-Mohammad@users.noreply.github.com>
2025-10-11 18:44:18 +0530

    Update bucket.tf

--- terraform/my_infra/bucket.tf
+++ terraform/my_infra/bucket.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket" "my_bucket" {
   bucket = "${var.env}-bucket3-${random_id.suffix.hex}"
 
   tags = {
-    Name        = "$${var.env}-bucket3-${random_id.suffix.hex}"
+    Name        = "${var.env}-bucket3-${random_id.suffix.hex}"
     Environment = var.env
   }
  }
